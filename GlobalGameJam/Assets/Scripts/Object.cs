123f2ea8853f84a404d2f492a6fe80b59d4569c5
Nathan-Boucher <59613211+Nathan-Boucher@users.noreply.github.com>
2024-01-22 15:25:37 +0100

    ProtoDragAndDrop

---
+++ GlobalGameJam/Assets/Scripts/Object.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public static Object currentObject;
+    private bool selected;
+    private Vector3 offset;
+    private Collider2D Collider2D;
+
+    void Start()
+    {
+        Collider2D = GetComponent<Collider2D>();
+    }
+    private void OnMouseDown()
+    {
+        offset = transform.position - MousePositionScreen();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = MousePositionScreen() + offset;
+    }
+
+    private void OnMouseUp()
+    {
+        Collider2D.enabled = false;
+        var rayOrigin = Camera.main.transform.position;
+        var rayDirection = MousePositionScreen() - Camera.main.transform.position;
+        RaycastHit2D hit;
+        if (hit = Physics2D.Raycast(rayOrigin, rayDirection))
+        {
+            if (hit.transform.CompareTag("DropItem"))
+            {
+                transform.position = hit.transform.position;
+            }
+        }
+
+        Collider2D.enabled = true;
+    }
+
+    Vector3 MousePositionScreen()
+    {
+        var mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+    }
+        
+}
