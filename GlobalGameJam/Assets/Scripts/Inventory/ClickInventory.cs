cb8fe68ce3be3f0c67b0265ff7db7db4712fb6f5
Nathan-Boucher <59613211+Nathan-Boucher@users.noreply.github.com>
2024-01-23 16:48:12 +0100

    Collide

--- GlobalGameJam/Assets/Scripts/Inventory/ClickInventory.cs
+++ GlobalGameJam/Assets/Scripts/Inventory/ClickInventory.cs
@@ -4,9 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 public class ClickInventory : MonoBehaviour
 {
+    [SerializeField] private GameObject menuDeroulant,hand;
+    [SerializeField] private bool stateInventory;
     public static event Action<int> selectItem; 
     public void OnClickSlot(int index)
     {
         selectItem?.Invoke(index);
+    }
+
+    public void HandInventory()
+    {
+        stateInventory = !stateInventory;
+        if (stateInventory)
+        {
+            menuDeroulant.SetActive(true);
+            hand.SetActive(false);
+        }
+        else
+        {
+            menuDeroulant.SetActive(false);
+            hand.SetActive(true);
+        }
     }
 }
