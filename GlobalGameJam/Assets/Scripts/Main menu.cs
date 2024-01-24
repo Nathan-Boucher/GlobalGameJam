d0cdc5728aa79c3262f880b37aa1a5bb6c432325
mrsurnom <145198693+mrsurnom@users.noreply.github.com>
2024-01-24 13:26:37 +0100

    quit button working and option almost

--- GlobalGameJam/Assets/Scripts/Main menu.cs
+++ GlobalGameJam/Assets/Scripts/Main menu.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
+    [SerializeField] private GameObject main,options;
 public void PlayGame()
 {
     SceneManager.LoadScene("MoveObject");
 }
+public void Options()
+{
+    main.SetActive(false);
+    options.SetActive(true);
+}
+
+public void QuitGame()
+{
+    Application.Quit();
+}
+
 }
